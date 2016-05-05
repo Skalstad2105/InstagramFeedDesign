0c0f83e377a6ad06c480c1438f1dee4a4990d0ff
Marius Skalstad <Skalstad10@gmail.com>
2016-05-05 09:40:33 +0200

    Initial Commit

---
+++ InstagramFeedDesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFeedDesign
+//
+//  Created by Marius Skalstad on 05.05.2016.
+//  Copyright © 2016 Marius Skalstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
